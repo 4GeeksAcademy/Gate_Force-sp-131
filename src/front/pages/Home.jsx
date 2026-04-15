3ff559be4fc6e598285b7f9bab2d71420d735d8a
Luis Sarmiento <48292291+dudunesto@users.noreply.github.com>
2026-04-15 19:11:03 +0000

    Admin creado

--- src/front/pages/Home.jsx
+++ src/front/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react"
 import rigoImageUrl from "../assets/img/rigo-baby.jpg";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
+import { Link } from "react-router-dom";
 
 export const Home = () => {
 
@@ -35,7 +36,11 @@ export const Home = () => {
 	return (
 		<div className="text-center mt-5">
 			<h1 className="display-4">Hello Rigo!!</h1>
+			<Link to="/admin">
+				<button className="btn btn-primary btn-lg">Ir a Admin</button>
+			</Link>
 			<p className="lead">
+
 				<img src={rigoImageUrl} className="img-fluid rounded-circle mb-3" alt="Rigo Baby" />
 			</p>
 			<div className="alert alert-info">
